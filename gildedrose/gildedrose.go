3873603eadbdae0f7198a89e2b066d8df354f4e3
Gintaras Aniulis <gintarasa95@gmail.com>
2024-02-10 09:54:49 +0200

    Refactor UpdateQuality

--- gildedrose/gildedrose.go
+++ gildedrose/gildedrose.go
@@ -7,55 +7,65 @@ type Item struct {
 
 func UpdateQuality(items []*Item) {
 	for i := 0; i < len(items); i++ {
+		updateItem(items[i])
+	}
+}
 
-		if items[i].Name != "Aged Brie" && items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
-			if items[i].Quality > 0 {
-				if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-					items[i].Quality--
-				}
-			}
-		} else {
-			if items[i].Quality < 50 {
-				items[i].Quality++
-
-				if items[i].Name == "Backstage passes to a TAFKAL80ETC concert" && items[i].Quality < 50 {
-					if items[i].SellIn < 11 {
-						items[i].Quality++
-					}
-					if items[i].SellIn < 6 {
-						items[i].Quality++
-					}
-				}
-			}
+func updateItem(item *Item) {
+	if item.Name == "Aged Brie" {
+		if item.Quality < 50 {
+			item.Quality++
 		}
-
-		if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-			items[i].SellIn--
-		}
-
-		if items[i].SellIn >= 0 {
-			continue
-		}
-
-		if items[i].Name == "Aged Brie" {
-			if items[i].Quality > 49 {
-				continue
-			}
-
-			items[i].Quality++
-			continue
-		}
-
-		if items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
-			if items[i].Quality > 0 {
-				if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-					items[i].Quality--
-				}
-			}
-		} else {
-			items[i].Quality = 0
-		}
-
 	}
 
+	if item.Name == "Backstage passes to a TAFKAL80ETC concert" {
+		if item.Quality < 50 {
+			item.Quality++
+		}
+
+		if item.Quality < 50 {
+			if item.SellIn < 11 {
+				item.Quality++
+			}
+
+			if item.SellIn < 6 {
+				item.Quality++
+			}
+		}
+	}
+
+	if item.Name != "Aged Brie" &&
+		item.Name != "Backstage passes to a TAFKAL80ETC concert" &&
+		item.Name != "Sulfuras, Hand of Ragnaros" &&
+		item.Quality > 0 {
+		item.Quality--
+	}
+
+	if item.Name != "Sulfuras, Hand of Ragnaros" {
+		item.SellIn--
+	}
+
+	if item.SellIn >= 0 {
+		return
+	}
+
+	if item.Name == "Aged Brie" {
+		if item.Quality > 49 {
+			return
+		}
+
+		item.Quality++
+
+		return
+	}
+
+	if item.Name == "Backstage passes to a TAFKAL80ETC concert" {
+		item.Quality = 0
+	}
+
+	if item.Name != "Backstage passes to a TAFKAL80ETC concert" &&
+		item.Name != "Sulfuras, Hand of Ragnaros" &&
+		item.Quality > 0 {
+		item.Quality--
+	}
 }
