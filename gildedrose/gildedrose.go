293fd629bdd353d592899fe3e34eec1e2fdcf996
Gintaras Aniulis <gintarasa95@gmail.com>
2024-02-10 08:56:23 +0200

    Refactor UpdateQuality

--- gildedrose/gildedrose.go
+++ gildedrose/gildedrose.go
@@ -11,48 +11,51 @@ func UpdateQuality(items []*Item) {
 		if items[i].Name != "Aged Brie" && items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
 			if items[i].Quality > 0 {
 				if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-					items[i].Quality = items[i].Quality - 1
+					items[i].Quality--
 				}
 			}
 		} else {
 			if items[i].Quality < 50 {
-				items[i].Quality = items[i].Quality + 1
-				if items[i].Name == "Backstage passes to a TAFKAL80ETC concert" {
+				items[i].Quality++
+
+				if items[i].Name == "Backstage passes to a TAFKAL80ETC concert" && items[i].Quality < 50 {
 					if items[i].SellIn < 11 {
-						if items[i].Quality < 50 {
-							items[i].Quality = items[i].Quality + 1
-						}
+						items[i].Quality++
 					}
 					if items[i].SellIn < 6 {
-						if items[i].Quality < 50 {
-							items[i].Quality = items[i].Quality + 1
-						}
+						items[i].Quality++
 					}
 				}
 			}
 		}
 
 		if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-			items[i].SellIn = items[i].SellIn - 1
+			items[i].SellIn--
 		}
 
-		if items[i].SellIn < 0 {
-			if items[i].Name != "Aged Brie" {
-				if items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].Quality > 0 {
-						if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-							items[i].Quality = items[i].Quality - 1
-						}
-					}
-				} else {
-					items[i].Quality = items[i].Quality - items[i].Quality
-				}
-			} else {
-				if items[i].Quality < 50 {
-					items[i].Quality = items[i].Quality + 1
+		if items[i].SellIn >= 0 {
+			continue
+		}
+
+		if items[i].Name == "Aged Brie" {
+			if items[i].Quality > 49 {
+				continue
+			}
+
+			items[i].Quality++
+			continue
+		}
+
+		if items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
+			if items[i].Quality > 0 {
+				if items[i].Name != "Sulfuras, Hand of Ragnaros" {
+					items[i].Quality--
 				}
 			}
+		} else {
+			items[i].Quality = 0
 		}
+
 	}
 
 }
