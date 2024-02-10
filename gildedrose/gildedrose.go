6c29047dbe958f72c4aa5923294c0f7db9aa744c
Gintaras Aniulis <gintarasa95@gmail.com>
2024-02-10 10:37:33 +0200

    Refactor UpdateQuality

--- gildedrose/gildedrose.go
+++ gildedrose/gildedrose.go
@@ -12,12 +12,6 @@ func UpdateQuality(items []*Item) {
 }
 
 func updateItem(item *Item) {
-	if item.Name == "Aged Brie" {
-		if item.Quality < 50 {
-			item.Quality++
-		}
-	}
-
 	if item.Name == "Backstage passes to a TAFKAL80ETC concert" {
 		if item.Quality < 50 {
 			item.Quality++
@@ -32,40 +26,43 @@ func updateItem(item *Item) {
 				item.Quality++
 			}
 		}
-	}
 
-	if item.Name != "Aged Brie" &&
-		item.Name != "Backstage passes to a TAFKAL80ETC concert" &&
-		item.Name != "Sulfuras, Hand of Ragnaros" &&
-		item.Quality > 0 {
-		item.Quality--
-	}
-
-	if item.Name != "Sulfuras, Hand of Ragnaros" {
 		item.SellIn--
+
+		if item.SellIn < 0 {
+			item.Quality = 0
+		}
+
+		return
 	}
 
-	if item.SellIn >= 0 {
+	if item.Name == "Sulfuras, Hand of Ragnaros" {
 		return
 	}
 
 	if item.Name == "Aged Brie" {
-		if item.Quality > 49 {
-			return
+		item.SellIn--
+		item.Quality++
+
+		if item.SellIn < 0 {
+			item.Quality++
 		}
 
-		item.Quality++
+		if item.Quality > 50 {
+			item.Quality = 50
+		}
 
 		return
 	}
 
-	if item.Name == "Backstage passes to a TAFKAL80ETC concert" {
-		item.Quality = 0
+	item.SellIn--
+
+	if item.Quality > 0 {
+		item.Quality--
 	}
 
-	if item.Name != "Backstage passes to a TAFKAL80ETC concert" &&
-		item.Name != "Sulfuras, Hand of Ragnaros" &&
-		item.Quality > 0 {
+	if item.Quality > 0 &&
+		item.SellIn < 0 {
 		item.Quality--
 	}
 }
