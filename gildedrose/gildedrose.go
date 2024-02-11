4eb5e12f8b29a4b4353c7d2c3fde466d5375ad45
Gintaras Aniulis <gintarasa95@gmail.com>
2024-02-11 14:12:54 +0200

    Refactor UpdateQuality

--- gildedrose/gildedrose.go
+++ gildedrose/gildedrose.go
@@ -6,21 +6,100 @@ type Item struct {
 }
 
 func UpdateQuality(items []*Item) {
+	lastHandler := &GenericHandler{}
+
+	thirdHandler := &AgedBrieHandler{}
+	thirdHandler.setNext(lastHandler)
+
+	secondHandler := &SulfurasHandler{}
+	secondHandler.setNext(thirdHandler)
+
+	firstHandler := &BackstagePassesHandler{}
+	firstHandler.setNext(secondHandler)
+
 	for i := 0; i < len(items); i++ {
-		switch items[i].Name {
-		case "Backstage passes to a TAFKAL80ETC concert":
-			updateBackstagePasses(items[i])
-		case "Sulfuras, Hand of Ragnaros":
-			updateSulfuras(items[i])
-		case "Aged Brie":
-			updateAgedBrie(items[i])
-		default:
-			update(items[i])
-		}
+		firstHandler.update(items[i])
 	}
 }
 
-func updateBackstagePasses(item *Item) {
+type ItemHandler interface {
+	update(item *Item)
+	setNext(ItemHandler)
+}
+
+type GenericHandler struct {
+	next ItemHandler
+}
+
+func (u *GenericHandler) update(item *Item) {
+	item.SellIn--
+
+	if item.Quality > 0 {
+		item.Quality--
+	}
+
+	if item.Quality > 0 && item.SellIn < 0 {
+		item.Quality--
+	}
+}
+
+func (u *GenericHandler) setNext(next ItemHandler) {
+	u.next = next
+}
+
+type SulfurasHandler struct {
+	next ItemHandler
+}
+
+func (u *SulfurasHandler) update(item *Item) {
+	if item.Name != "Sulfuras, Hand of Ragnaros" {
+		u.next.update(item)
+	}
+}
+
+func (u *SulfurasHandler) setNext(next ItemHandler) {
+	u.next = next
+}
+
+type AgedBrieHandler struct {
+	next ItemHandler
+}
+
+func (u *AgedBrieHandler) update(item *Item) {
+	if item.Name != "Aged Brie" {
+		u.next.update(item)
+
+		return
+	}
+
+	item.SellIn--
+
+	item.Quality++
+
+	if item.SellIn < 0 {
+		item.Quality++
+	}
+
+	if item.Quality > 50 {
+		item.Quality = 50
+	}
+}
+
+func (u *AgedBrieHandler) setNext(next ItemHandler) {
+	u.next = next
+}
+
+type BackstagePassesHandler struct {
+	next ItemHandler
+}
+
+func (u *BackstagePassesHandler) update(item *Item) {
+	if item.Name != "Backstage passes to a TAFKAL80ETC concert" {
+		u.next.update(item)
+
+		return
+	}
+
 	item.SellIn--
 
 	if item.Quality < 50 {
@@ -42,32 +121,6 @@ func updateBackstagePasses(item *Item) {
 	}
 }
 
-func updateSulfuras(item *Item) {
-
-}
-
-func updateAgedBrie(item *Item) {
-	item.SellIn--
-
-	item.Quality++
-
-	if item.SellIn < 0 {
-		item.Quality++
-	}
-
-	if item.Quality > 50 {
-		item.Quality = 50
-	}
-}
-
-func update(item *Item) {
-	item.SellIn--
-
-	if item.Quality > 0 {
-		item.Quality--
-	}
-
-	if item.Quality > 0 && item.SellIn < 0 {
-		item.Quality--
-	}
+func (u *BackstagePassesHandler) setNext(next ItemHandler) {
+	u.next = next
 }
